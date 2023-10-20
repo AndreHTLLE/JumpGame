865687b5eb2138cd6782e17ba75bfcb9ea264604
andre <211wita19@o365.htl-leoben.at>
2023-10-20 13:22:22 +0200

    menü

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -6,12 +6,27 @@ import java.awt.event.*;
 public class Game extends JPanel implements ActionListener {
     private final Timer timer;
 
+    public static GameObject exitButton;
+    public static GameObject startButton;
+    public static int frameWidth=500;
+    public static int frameHeight=100;
+
+    public static JFrame frame;
 
     public Game() {
-        this.setPreferredSize(new Dimension(800, 600));
+
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+
+        int x = (int) ((screenSize.getWidth()));
+        int y = (int) ((screenSize.getHeight()));
+        this.setPreferredSize(new Dimension(x, y));
         this.timer = new Timer(16, this);  // 60 FPS
 
 
+        exitButton = new GameObject(200, 200, 400, 400, "src/main/java/Images/ExitBUTTON.png", true);
+        startButton = new GameObject(1100, 200, 400, 400, "src/main/java/Images/StartBUTTON.png", true);
+        exitButton.setClickable(true);
+        startButton.setClickable(true);
 
 
         addKeyListener(new KeyAdapter() {
@@ -50,6 +65,16 @@ public class Game extends JPanel implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
 
+        if(exitButton.isClicked()) {
+            exitButton.setClicked(false);
+            System.exit(0);
+        }
+
+
+        if(startButton.isClicked()) {
+            startButton.setClicked(false);
+
+        }
 
         repaint();
     }
@@ -58,24 +83,20 @@ public class Game extends JPanel implements ActionListener {
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-
+        startButton.draw(g,this);
+        exitButton.draw(g,this);
     }
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("GameObject Test");
+        frame = new JFrame("Menü");
         frame.setBackground(Color.CYAN);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLocation(800,500);
+        frame.setLocation(frameWidth,frameHeight);
         frame.setContentPane(new Game());
         frame.pack();
         frame.setVisible(true);
     }
-
-    public static void startScreen() {
-
-
-
-    }
 }
+
 
 
